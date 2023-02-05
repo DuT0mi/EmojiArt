93b4d70236fa072747a2e41071624f976c118e0d
DuTomi <dudas.tamas.alex@gmail.com>
2023-02-05 16:53:27 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Dudas Tamas Alex on 2023. 02. 05..
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
