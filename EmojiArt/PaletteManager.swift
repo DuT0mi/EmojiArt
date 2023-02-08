4eaf34c678ae4703ffa6f5ab3b1d8ec267c29297
DuTomi <dudas.tamas.alex@gmail.com>
2023-02-08 16:20:41 +0100

    Examples of injecting environment variables with binding

--- EmojiArt/PaletteManager.swift
+++ EmojiArt/PaletteManager.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct PaletteManager: View {
     // It is editing the palette store so we definetely need it -> @Enviroment
     @EnvironmentObject var store: PaletteStore
+    @Environment (\.colorScheme) var colorScheme
     
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
@@ -18,18 +20,24 @@ struct PaletteManager: View {
                 ForEach(store.palettes){palette in
                     NavigationLink(destination: PaletteEditor(palette: $store.palettes[palette])){
                         VStack(alignment: .leading){
-                            Text(palette.name)
+                            Text(palette.name).font(colorScheme == .dark ? .caption : .largeTitle)
                             Text(palette.emojis)
                         }
                     }
                 }
             }
+            .navigationTitle("Manage Palettes")
+            .navigationBarTitleDisplayMode(.inline)
+           // .environment(\.colorScheme, .dark)
+            .environment(\.editMode, $editMode)
         }
     }
 }
 
 struct PaletteManager_Previews: PreviewProvider {
     static var previews: some View {
-        PaletteManager().environmentObject(PaletteStore(named: "Preview"))
+        PaletteManager()
+            .environmentObject(PaletteStore(named: "Preview"))
+            
     }
 }
