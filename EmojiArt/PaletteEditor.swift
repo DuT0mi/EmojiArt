d9d23ac475e393db4926456787c43e4dd37ad80d
DuTomi <dudas.tamas.alex@gmail.com>
2023-02-08 15:43:14 +0100

    Able to remove emojis and adding

--- EmojiArt/PaletteEditor.swift
+++ EmojiArt/PaletteEditor.swift
@@ -12,16 +12,54 @@ struct PaletteEditor: View {
     
     var body: some View {
         Form{// For that cool looking
-            TextField("Name",text: $palette.name)// Must to bind, coz the TextField can not work with that plain string
+            nameSection
+            addEmojisSection
+            removeEmojiSection
         }
         .frame(minWidth:300, minHeight: 350)
+    }
+    var nameSection: some View{
+        Section(header:Text("Name")){
+            TextField("Name",text: $palette.name)// Must to bind, coz the TextField can not work with that plain string
+        }
+    }
+    @State private var emojisToAdd = ""
+    var addEmojisSection: some View {
+        Section(header: Text("Add emojis")){
+            TextField("",text: $emojisToAdd)
+                .onChange(of: emojisToAdd){emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    func addEmojis(_ emojis:String){
+        withAnimation{
+            palette.emojis = (emojis + palette.emojis)
+                .filter {$0.isEmoji}
+                .removingDuplicateCharacters
+        }
+    }
+    var removeEmojiSection: some View {
+        Section(header: Text("Remove Emoji")) {
+            let emojis = palette.emojis.removingDuplicateCharacters.map { String($0) }
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .onTapGesture {
+                            withAnimation {
+                                palette.emojis.removeAll(where: { String($0) == emoji })
+                            }
+                        }
+                }
+            }
+            .font(.system(size: 40))
+        }
     }
 }
 
 struct PaletteEditor_Previews: PreviewProvider {
     static var previews: some View {
-        Text("Fix me")
-      //  PaletteEditor()
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))// constant bindig, coz we do not have any one of it
             .previewLayout(.fixed(width: 300, height: 300))
     }
 }
