38e6644476ab6365a5239283a5af12b61156bc6b
DuTomi <dudas.tamas.alex@gmail.com>
2023-02-11 14:58:08 +0100

    Fixed the buttons for each platform (ellipsis only showing itself when there is not any space for those buttons)

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -72,13 +72,27 @@ struct EmojiArtDocumentView: View {
                     zoomToFit(image, in: geometry.size)
                 }
             }
-            .toolbar {
-                UndoButton(
-                undo: undoManager?.optionalUndoMenuItemTitle,
-                redo: undoManager?.optionalRedoMenuItemTitle
-                )
+            .compactableToolbar {
+                    AnimatedActionButton(title: "Paste Background",systemImage: "doc.on.clipboard"){
+                        pasteBackground()
+                    }
+                    if let undoManager = undoManager {
+                        if undoManager.canUndo{
+                            AnimatedActionButton(title:undoManager.undoActionName,systemImage: "arrow.uturn.backward"){
+                                undoManager.undo()
+                            }
+                        }
+                        if undoManager.canRedo{
+                            AnimatedActionButton(title:undoManager.redoActionName,systemImage: "arrow.uturn.forward"){
+                                undoManager.redo()
+                            }
+                        }
+                    }
             }
         }
+    }
+    private func pasteBackground(){
+        
     }
     private func doubleTapToZoom(in size: CGSize)->some Gesture{
         TapGesture(count: 2)
